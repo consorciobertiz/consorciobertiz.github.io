b9610312f96cdab2297b07003e12e9f7841d6c1d
David Gil <david.gil@biko2.com>
2013-09-25 14:34:29 +0200

    favicons y pequeños ajustes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+---
+---
+{% include js/jquery-1.10.1.min.js %};
+{% include js/bootstrap.min.js%};
 $('a.restaurant').popover();
 
 $('a.restaurant').on('click', function (e) {
-    $('a.restaurant').not(this).popover('hide');
+  e.preventDefault();
+  $('a.restaurant').not(this).popover('hide');
 });
