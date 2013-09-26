353b0c8bd6345475cbf5de4aa04859eacb908dca
David Gil <david.gil@biko2.com>
2013-09-26 11:17:12 +0200

    ajustados los eventos de javascript para los popovers

--- js/main.js
+++ js/main.js
@@ -2,15 +2,25 @@
 ---
 {% include js/jquery-1.10.1.min.js %};
 {% include js/bootstrap.min.js%};
-$('a.restaurant').popover();
-
-$('a.restaurant').on('click', function (e) {
-  e.preventDefault();
-  $('a.restaurant').not(this).popover('hide');
-});
 
 $('a#lnk_splash').on('click', function (e) {
   e.preventDefault();
   $('section.splash').removeClass("visible-xs");
   $('section.splash').hide(500);
+});
+
+$('a.restaurant').popover();
+$('a.restaurant').on('click', function (e) {
+  e.preventDefault();
+
+  $('a.restaurant').not(this).each(function() {
+      var element = $(this),
+          self = element.data('bs.popover');
+
+      if(self.tip().hasClass('in')) {
+        // There are some issues in `.popover('hide')`
+        // hence use internal function `leave`
+        self.leave(self);
+      }
+    });
 });
